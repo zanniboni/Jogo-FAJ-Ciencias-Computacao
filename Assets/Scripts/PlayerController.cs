68e011a6c682e0536e0e7400ea64767a904615fd
Vinicius Zaniboni Chagas <viny.zanchagas@gmail.com>
2021-03-27 15:50:18 -0300

    Bug fixes & Animation add
    Adicionado animação de abaixar através do CTRL, corrigido bug das animações de up, down e sides.
    Adicionado captura de teclas do teclado para determinação de ações.
    Adicionado animação jump_1 para utilizar futuramente
    Adicionado animação shoot_1 para utilizar futuramente

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    
     Rigidbody2D myRB;
     Transform myAvatar;
     Animator myAnim;
@@ -22,63 +23,112 @@ public class PlayerController : MonoBehaviour
     {
         WASD.Disable();
     }
+
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
         myAvatar = transform.GetChild(0);
-
         myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        myAnim.ResetTrigger("key_down");
-        myAnim.ResetTrigger("key_up");
+        
+        var keyboard = Keyboard.current;
+        var apertou_w = keyboard.wKey.isPressed;
+        var apertou_s = keyboard.sKey.isPressed;
+        var apertou_d = keyboard.dKey.isPressed;
+        var apertou_a = keyboard.aKey.isPressed;
+        var apertou_space = keyboard.spaceKey.isPressed;
+        var apertou_ctrl = keyboard.ctrlKey.isPressed;
+
         movementInput = WASD.ReadValue<Vector2>();
-
-
-        if (movementInput.x > 0)
+        //Apertou W
+        if (apertou_w)
         {
-            myAnim.SetTrigger("key_side_right");
-            myAnim.ResetTrigger("key_side_left");
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+            ativarAnimacoes("apertou_w", apertou_w);
         } 
-
-        if(movementInput.x < 0)
+        else
         {
-            myAnim.ResetTrigger("key_side_right");
-            myAnim.SetTrigger("key_side_left");
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+            ativarAnimacoes("apertou_w", apertou_w);
         }
 
-        if (movementInput.y < 0)
+        //Apertou S
+        if (apertou_s)
         {
-            myAnim.ResetTrigger("key_up");
-            myAnim.SetTrigger("key_down");
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
-        }
-        if (movementInput.y > 0)
-        {
-            myAnim.ResetTrigger("key_down");
-            myAnim.SetTrigger("key_up");
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
-
+            ativarAnimacoes("apertou_s", apertou_s);
+        } 
+        else
+        { 
+            ativarAnimacoes("apertou_s", apertou_s); 
         }
 
+        //Apertou D
+        if (apertou_d)
+        {
+            ativarAnimacoes("apertou_d", apertou_d);
+        }
+        else
+        {
+            ativarAnimacoes("apertou_d", apertou_d);
+        }
 
+        //Apertou A
+        if (apertou_a)
+        {
+            ativarAnimacoes("apertou_a", apertou_a);
+        }
+        else
+        {
+            ativarAnimacoes("apertou_a", apertou_a);
+        }
 
-        myAnim.SetFloat("Speed", movementInput.magnitude);
+        //Apertou espaço
+        if (apertou_space)
+        {
+            ativarAnimacoes("apertou_space", apertou_space);
+        }
+        else
+        {
+            ativarAnimacoes("apertou_space", apertou_space);
+        }
 
+        //Apertou CTRL
+        if (apertou_ctrl)
+        {
+            ativarAnimacoes("abaixar", apertou_ctrl);
+        }
+        else
+        {
+            ativarAnimacoes("abaixar", apertou_ctrl);
+        }
+        
     }
 
 
+    private void ativarAnimacoes(string trigger, bool stateAnim)
+    { 
+        myAnim.SetBool(trigger, stateAnim);
+        
+        if(trigger == "apertou_a" && stateAnim)
+        {
+            myAvatar.localScale = new Vector2(Mathf.Sign(1), 1);
+        } 
 
+        if(trigger == "apertou_d" && stateAnim)
+        {
+            myAvatar.localScale = new Vector2(Mathf.Sign(-1), 1);
+        }
+
+
+        //myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+        myAnim.SetFloat("Speed", movementInput.magnitude);
+    }
 
     private void FixedUpdate()
     {
-
         myRB.velocity = movementInput * movementSpeed;
     }
 
